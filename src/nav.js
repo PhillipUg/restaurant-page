ff9226eecf676ca1cb384adffd0335ecd141e4d3
Phillip Musiime <phillipmusiime@gmail.com>
2020-08-23 00:01:41 +0300

    refactored createNav function

--- src/nav.js
+++ src/nav.js
@@ -1,9 +1,9 @@
 const createNav = () => {
+
+  let tabsArr = ['HOME', 'MENU', 'CONTACT &nbsp;&nbsp;&nbsp; US'];
+
   const navbar = document.createElement('nav');
   navbar.classList.add('navbar');
-
-  const logo = document.createElement('div');
-  logo.classList.add('logo');
 
   const logoText = document.createElement('div');
   logoText.classList.add('logo-text');
@@ -12,22 +12,13 @@ const createNav = () => {
   const tabs = document.createElement('div');
   tabs.classList.add('tabs');
 
-  const tab1 = document.createElement('button');
-  tab1.classList.add('tab');
-  tab1.innerText = 'HOME';
+  for (let i = 0, j = tabsArr.length; i < j; i += 1) {
+    const tab = document.createElement('button');
+    tab.classList.add('tab');
+    tab.innerHTML = tabsArr[i];
+    tabs.appendChild(tab);
+  }
 
-  const tab2 = document.createElement('button');
-  tab2.classList.add('tab');
-  tab2.innerText = 'MENU';
-
-  const tab3 = document.createElement('button');
-  tab3.classList.add('tab');
-  tab3.innerHTML = 'CONTACT &nbsp;&nbsp;&nbsp; US';
-
-  tabs.appendChild(tab1);
-  tabs.appendChild(tab2);
-  tabs.appendChild(tab3);
-  navbar.appendChild(logo);
   navbar.appendChild(logoText);
   navbar.appendChild(tabs);
 
